f1f21509d9091eda967d8a5554e602f2649e8535
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-08-08 10:15:54 -0400

    set up react router

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavBar() {
+    return (
+    <nav>
+        <h1>
+        America's Most Haunted
+        {/* <span className="logo" role="img"> :skull:  </span> */}
+        </h1>
+    </nav>
+    );
+}
+
+export default NavBar;
