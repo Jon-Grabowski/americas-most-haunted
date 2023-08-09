8066b6a82eae54a1f285fa34ffa88c6e06038968
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-08-09 10:40:39 -0400

    more css thangs and home info

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
 import About from "./About"
+import HauntedHouseList from "./HauntedHouseList";
+import HauntedHouseCard from "./HauntedHouseCard";
+import HauntedHouseDetail from "./HauntedHouseDetail";
 
 function Home() {
     return (
         <div>
-
-            <div className="about-section"><About/> </div>
+            {/* <div className="list-of-locations"> STUFF IN HERE </div> */}
+            <div className="about-section"><About/>
+            </div>
             
         </div>
+        
+        
     );
 }
 
