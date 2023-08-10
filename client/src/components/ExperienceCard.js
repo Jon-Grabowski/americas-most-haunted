1ec7bb6224c8b9678f3e1d15c08c76ed7d34757a
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-08-10 16:28:09 -0400

    experience buttons?

--- client/src/components/ExperienceCard.js
+++ client/src/components/ExperienceCard.js
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
+
 
 function ExperienceCard({visit}) {
+    // const[ editExperience , setEditExperience ] = useState("")
+    // const[ deleteExperience, setDeleteExperience ] = useState ("")
+
+    function handleEdit(e) {
+            // setEditExperience(e.target.value)
+            // console.log(e.target.value)
+        }
+    function handleDelete() {
+        console.log(visit.experience.id)
+        fetch(`/experiences/${visit.experience.id}`, {
+            method: "DELETE",
+        })
+    }
+    
+// function handleDelete() {
+//     fetch(`/experiences/${visit.experience.id}`, {
+//         method: "DELETE",     
+//     }) 
+//     .then((r)=> r.json())
+//     .then(() => console.log("HI!"))
+//     }
+
     return(
+
         <div className='ex-card-container'>
             <div className='ex-card-header'>
                 <p>{visit.user.username} visited on {visit.date}</p>
@@ -11,9 +37,12 @@ function ExperienceCard({visit}) {
             <div className='ex-card-rating'>
                 <p>Scary Rating: {visit.experience.rating} Screams!</p>
             </div>
-            {/* <div className="experience-button">
-                
-            </div> */}
+            <div className="edit-experience-btn">
+                <button onClick = {handleEdit}>Edit your experience 🦇 </button>
+            </div>
+            <div className="delete-experience-btn">
+                <button onClick = {handleDelete}>Delete your experience 🦇 </button>
+            </div>
         </div>
     )
 }
