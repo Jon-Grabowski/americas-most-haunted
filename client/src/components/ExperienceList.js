5e229bf1f6b831bb1ca7ca731e3ea5023236561b
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-08-10 14:26:58 -0400

    Add a visit button functional

--- client/src/components/ExperienceList.js
+++ client/src/components/ExperienceList.js
@@ -3,9 +3,11 @@ import ExperienceCard from "./ExperienceCard";
 function ExperienceList({house}) {
     console.log (house)
     const experienceList = house.visits.map((visit) => {
-        return(
-            <ExperienceCard key={visit.id} visit={visit}/>
-        )
+        if (visit.experience) {
+            return(
+                <ExperienceCard key={visit.id} visit={visit}/>
+            )
+        }
     }) 
 
     return(
