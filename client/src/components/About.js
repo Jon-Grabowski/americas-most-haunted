bb771dde86d09f57cee0ecbd41c18ac01ddd7983
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-08-10 09:44:17 -0400

    edited home page style

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,10 +6,10 @@ function About(){
             <div className="about-title"><h1>Welcome To America's Most Haunted!</h1>
             </div>
 
-
-            <p>'America's Most Haunted' is the spook-tacular online portal that will send shivers down your spine and make your ghostly encounters feel like a scream come true! Step into the realm of the unexplained as we take you on a hair-raising journey through the country's eeriest locales.</p>
-            <br></br>
-            We dare you to summon the courage to share your very own spectral saga. Whether it's a close encounter with a mischievous spirit, a bone-chilling brush with the unknown, or a firsthand account of a ghoulish gathering, our 'Add New Location' feature lets you contribute your own eerie experiences to our ever-expanding library of ghostly tales. Remember, the more you share, the more our spectral community grows – it's a hauntingly good time for all!
+                <div className="about-info"><p>'America's Most Haunted' is the spook-tacular online portal that will send shivers down your spine and make your ghostly encounters feel like a scream come true! Step into the realm of the unexplained as we take you on a hair-raising journey through the country's eeriest locales.</p>
+                            <br></br>
+                    We dare you to summon the courage to share your very own spectral saga. Whether it's a close encounter with a mischievous spirit, a bone-chilling brush with the unknown, or a firsthand account of a ghoulish gathering, our 'Add New Location' feature lets you contribute your own eerie experiences to our ever-expanding library of ghostly tales. Remember, the more you share, the more our spectral community grows – it's a hauntingly good time for all! </div>
+            
 
         </div>
     )
